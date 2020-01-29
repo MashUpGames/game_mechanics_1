9b43f3a5fa2ed6e4e235cf92c6b4673de65db7c0
Oliver Brown <36334944+TheMashupMann@users.noreply.github.com>
2020-01-29 22:47:41 +0000

    Added files

---
+++ GravityPoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode()]
+public class GravityPoint : MonoBehaviour
+{
+    public float gravityScale=12f, planetRadius=2f, gravityMinRange=1f, gravityMaxRange=2f;
+    public GameObject planet, minRS, maxRS;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        minRS.transform.localScale = new Vector3((planetRadius + gravityMinRange) * 2, (planetRadius + gravityMinRange) * 2, 1);
+        maxRS.transform.localScale = new Vector3((planetRadius + gravityMinRange + gravityMaxRange) * 2, (planetRadius + gravityMinRange + gravityMaxRange) * 2, 1);
+        GetComponent<CircleCollider2D>().radius = planetRadius + gravityMinRange + gravityMaxRange;
+        planet.transform.localScale = new Vector3(1f, 1f, 1f) * planetRadius * 2;
+    }
+
+    void OnTriggerStay2D(Collider2D obj)
+    {
+        float gravitationalPower = gravityScale / planetRadius;
+        float dist = Vector2.Distance(obj.transform.position, transform.position);
+
+        if (dist > (planetRadius + gravityMinRange))
+        {
+            float min = planetRadius + gravityMinRange + 0.5f;
+            gravitationalPower = gravitationalPower * (((min + gravityMaxRange) - dist) / gravityMaxRange);
+        }
+
+        Vector3 dir = (transform.position - obj.transform.position) * gravitationalPower;
+        obj.GetComponent<Rigidbody2D>().AddForce(dir);
+
+        if (obj.CompareTag("Player"))
+        {
+            obj.transform.up = Vector3.MoveTowards(obj.transform.up, -dir, gravitationalPower * Time.deltaTime * 5f);
+        }
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed=10f, jumpPower=10f;
+    public SpriteRenderer sprite;
+
+    Rigidbody2D body;
+    bool isGrounded;
+    float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            body.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isGrounded)
+        {
+            body.AddForce(transform.right * horizontal * moveSpeed);
+        }
+        sprite.flipX = horizontal > 0 ? false : (horizontal < 0 ? true : sprite.flipX);
+    }
+
+    void OnTriggerStay2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Planet"))
+        {
+            body.drag = 1f;
+
+            float distance = Mathf.Abs(obj.GetComponent<GravityPoint>().planetRadius - Vector2.Distance(transform.position, obj.transform.position));
+            if (distance < 1f)
+            {
+                isGrounded = distance < 0.5f;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Planet"))
+        {
+            body.drag = 0.2f;
+        }
+    }
+}
